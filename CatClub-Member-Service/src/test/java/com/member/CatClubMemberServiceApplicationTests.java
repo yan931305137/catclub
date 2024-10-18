9f67d4fc4e4e6eabcb0bbb7c8ea4c4cf01378b4f
yan931305137 <931305137@qq.com>
2024-10-18 11:03:14 +0800

    first commit

---
+++ CatClub-Member-Service/src/test/java/com/member/CatClubMemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatClubMemberServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
